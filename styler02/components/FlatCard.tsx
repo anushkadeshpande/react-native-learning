0367a2bf7610a52338d6c3c3a7b022d2658b05ab
anushkadeshpande <anushkaa5000@gmail.com>
2023-02-17 00:05:42 +0530

    feat: Added Flatcard component

---
+++ styler02/components/FlatCard.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const FlatCard = () => {
+  return (
+    <View>
+      <Text>FlatCard</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+
+})
+
+export default FlatCard
