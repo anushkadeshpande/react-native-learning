a9798aba0e9fe69e271feccb31b304c4c73d5f07
anushkadeshpande <anushkaa5000@gmail.com>
2023-02-18 02:49:59 +0530

    style: Styled the ActionCard component

--- styler02/components/ActionCard.tsx
+++ styler02/components/ActionCard.tsx
@@ -1,4 +1,4 @@
-import { Linking, StyleSheet, Text, View, Image, Touchable, TouchableOpacity } from 'react-native'
+import { SafeAreaView, Linking, StyleSheet, Text, View, Image, Touchable, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function ActionCard() {
@@ -8,12 +8,13 @@ export default function ActionCard() {
     }
 
   return (
+    <SafeAreaView>
     <View>
       <Text style={styles.headingText}>Blog Card</Text>
-      <View style={[styles.card]}>
-      <View style={[styles.headerContainer]}></View>
-      <View style={[styles.headerText]}>
-        <Text>Dummy header text</Text>
+      <View style={styles.card}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Dummy header text</Text>
+      
       </View>
 
       <Image 
@@ -25,7 +26,7 @@ export default function ActionCard() {
       />
 
       <View style={styles.bodyContainer}>
-        <Text numberOfLines={3}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac tellus nec ligula consectetur volutpat tincidunt ac diam. Donec tincidunt interdum bibendum. Donec ultricies eget odio eu molestie. Integer iaculis.</Text>
+        <Text numberOfLines={3} style={styles.bodyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac tellus nec ligula consectetur volutpat tincidunt ac diam. Donec tincidunt interdum bibendum. Donec ultricies eget odio eu molestie. Integer iaculis.</Text>
       </View>
 
 
@@ -36,6 +37,7 @@ export default function ActionCard() {
         </View>
       </View>
     </View>
+    </SafeAreaView>
   )
 }
 
@@ -47,22 +49,44 @@ const styles = StyleSheet.create({
        }, 
 
        card: {
-
+            width: 350,
+            height: 400,
+            borderRadius: 10,
+            marginVertical: 12,
+            marginHorizontal: 16,
+            backgroundColor: '#fff'
        },
 
        headerContainer: {
-
+            height: 40,
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
        },
 
-       headerText: {},
+       headerText: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: '600'
+       },
 
        cardImage: {
-        height: 250
+        height: 250,
+        borderRadius: 10
        },
 
-       bodyContainer: {},
+       bodyContainer: {
+        padding: 10
+    },
 
-       footerContainer: {},
+       bodyText: {
+        color: '#000',
+       },
+
+       footerContainer: {
+        // color: '#000', 
+       },
 
        socialLink: {
         padding: 5,
@@ -71,6 +95,9 @@ const styles = StyleSheet.create({
         margin: 10,
         textAlign: 'center',
         fontWeight: 'bold',
-        borderRadius: 10
+        borderRadius: 10,
+        // flex: 1,
+        // alignItems: 'center'
+        alignSelf: 'center',
        }
 })
