290581ab08b58379dccd4739e5ac253054322339
anushkadeshpande <anushkaa5000@gmail.com>
2023-03-19 00:16:48 +0530

    added image prop

--- rollthedice05/src/App.tsx
+++ rollthedice05/src/App.tsx
@@ -8,6 +8,8 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Image,
+  ImageSourcePropType,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -17,14 +19,31 @@ import {
   View,
 } from 'react-native';
 
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
 
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
 
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  )
+}
 
 function App(): JSX.Element {
 
   return (
     <View>
       <Text>Text</Text>
+      <Dice imageUrl={DiceOne}/>
     </View>
   );
 }
